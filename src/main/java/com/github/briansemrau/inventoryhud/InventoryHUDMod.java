5385b339cd2e22ce3f69a07ce1f12936b2313bb1
Brian Semrau <brian.semrau@gmail.com>
2019-06-23 17:50:30 -0400

    Initial commit

---
+++ src/main/java/com/github/briansemrau/inventoryhud/InventoryHUDMod.java
@@ -0,0 +1,13 @@
+package com.github.briansemrau.inventoryhud;
+
+import net.fabricmc.api.ModInitializer;
+
+public class InventoryHUDMod implements ModInitializer {
+
+	public static final String MOD_ID = "inventory_hud";
+
+	@Override
+	public void onInitialize() {
+	}
+
+}
