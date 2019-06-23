70c1bf6af41357a3f6c2c1a65a0e319d55f22a8b
Brian Semrau <brian.semrau@gmail.com>
2019-06-23 17:58:04 -0400

    Clean up code

--- src/main/java/com/github/briansemrau/inventoryhud/mixin/InGameHudMixin.java
+++ src/main/java/com/github/briansemrau/inventoryhud/mixin/InGameHudMixin.java
@@ -4,16 +4,11 @@ import com.mojang.blaze3d.platform.GlStateManager;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.ClientChatListener;
 import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.hud.*;
-import net.minecraft.client.options.AttackIndicator;
 import net.minecraft.client.render.GuiLighting;
-import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.network.chat.ChatMessageType;
-import net.minecraft.util.AbsoluteHand;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -22,87 +17,30 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
-
 @Environment(EnvType.CLIENT)
 @Mixin(InGameHud.class)
 public abstract class InGameHudMixin extends DrawableHelper {
 
-    private static final Identifier BACKGROUND_TEXTURE = new Identifier("textures/gui/container/inventory.png");
+    private static final Identifier INVENTORY_TEX = new Identifier("textures/gui/container/inventory.png");
 
     @Shadow
     @Final
-    private static Identifier WIDGETS_TEX;
-    @Shadow
-    @Final
-    private Random random;
-    @Shadow
-    @Final
     private MinecraftClient client;
-    @Shadow
-    @Final
-    private ItemRenderer itemRenderer;
-    @Shadow
-    @Final
-    private ChatHud chatHud;
-    @Shadow
-    private int ticks;
-    @Shadow
-    private String overlayMessage = "";
-    @Shadow
-    private int overlayRemaining;
-    @Shadow
-    private boolean overlayTinted;
-    @Shadow
-    public float field_2013 = 1.0F;
-    @Shadow
-    private int heldItemTooltipFade;
-    @Shadow
-    private ItemStack currentStack;
-    @Shadow
-    @Final
-    private DebugHud debugHud;
-    @Shadow
-    @Final
-    private SubtitlesHud subtitlesHud;
-    @Shadow
-    @Final
-    private SpectatorHud spectatorHud;
-    @Shadow
-    @Final
-    private PlayerListHud playerListHud;
-    @Shadow
-    @Final
-    private BossBarHud bossBarHud;
-    @Shadow
-    private int titleTotalTicks;
-    @Shadow
-    private String title;
-    @Shadow
-    private String subtitle;
-    @Shadow
-    private int titleFadeInTicks;
-    @Shadow
-    private int titleRemainTicks;
-    @Shadow
-    private int titleFadeOutTicks;
-    @Shadow
-    private int field_2014;
-    @Shadow
-    private int field_2033;
-    @Shadow
-    private long field_2012;
-    @Shadow
-    private long field_2032;
+
     @Shadow
     private int scaledWidth;
     @Shadow
     private int scaledHeight;
+
     @Shadow
-    @Final
-    private Map<ChatMessageType, List<ClientChatListener>> listeners;
+    private PlayerEntity getCameraPlayer() {
+        return null; // method content ignored
+    }
+
+    @Shadow
+    private void renderHotbarItem(int int_1, int int_2, float float_1, PlayerEntity playerEntity_1, ItemStack itemStack_1) {
+        // method content ignored
+    }
 
     @Inject(method = "draw", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;renderHotbar(F)V"))
     public void onDraw(float float_1, CallbackInfo ci) {
@@ -114,7 +52,7 @@ public abstract class InGameHudMixin extends DrawableHelper {
         if (playerEntity != null) {
             GlStateManager.color4f(1.0F, 1.0F, 1.0F, 0.6F);
 
-            this.client.getTextureManager().bindTexture(BACKGROUND_TEXTURE);
+            this.client.getTextureManager().bindTexture(INVENTORY_TEX);
             int padding = 5;
             int texWidth = 162;
             int texHeight = 54;
@@ -171,15 +109,6 @@ public abstract class InGameHudMixin extends DrawableHelper {
             GlStateManager.disableRescaleNormal();
 //            GlStateManager.disableBlend();
         }
-    }
-
-    @Shadow
-    private PlayerEntity getCameraPlayer() {
-        return null;
-    }
-
-    @Shadow
-    private void renderHotbarItem(int int_1, int int_2, float float_1, PlayerEntity playerEntity_1, ItemStack itemStack_1) {
     }
 
 }
